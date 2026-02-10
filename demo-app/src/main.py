72740d31447d469fcf9461596ce1d0fd8cabedc4
reecelikesramen <3149587+reecelikesramen@users.noreply.github.com>
2026-02-10 13:12:43 -0700

    feat: bump examples to align with new syntax

--- demo-app/src/main.py
+++ demo-app/src/main.py
@@ -1,8 +1,13 @@
 """Main entry point for demo app."""
 from pywire import PyWire
+from pathlib import Path
+
+# Get the pages directory
+pages_dir = Path(__file__).parent / "pages"
 
 # Create app instance
 app = PyWire(
+    pages_dir = str(pages_dir),
     path_based_routing=False,
     static_dir="static",
     debug=True
